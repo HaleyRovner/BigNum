950b54425e0dd99d93b7dd292ca110960c7cdb33
Haley <57505566+HaleyRovner@users.noreply.github.com>
2021-09-15 12:48:52 -0700

    Add files via upload

---
+++ BigNum.java
@@ -0,0 +1,14 @@
+public class BigNum {
+    public static void BigNum(String arg){
+
+    }
+    public static String toString(double num){
+
+    }
+    public static double plus(double numOne, double numTwo){
+
+    }
+    public static double times(double numOne, double numTwo){
+
+    }
+}
